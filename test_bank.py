d64f662b42a328b50ccffa15324996fad9e0e394
Daksha-Belawadi <belawadidaksha@gmail.com>
2026-01-08 17:36:45 +0530

    Update test_bank.py

--- test_bank.py
+++ test_bank.py
@@ -2,10 +2,10 @@ from bank import bank_details
 
 def test_bank_details():
     expected_output = (
-        "Account_number = 515\n"
-        "Account_holder_name = Daksha\n"
-        "Account_type = Current\n"
-        "Balance = 50000\n"
+        "Account_number=515\n"
+        "Account_holder_name=Daksha\n"
+        "Account_type=Current\n"
+        "Balance=50000\n"
     )
 
-    assert bank_details(515, "Daksha", "Current", 50000) == expected_output
+    assert bank_details(515,"Daksha","Current",50000)==expected_output
